b380f7a7a302a71604d2464b4034f6671526de68
Tom Bond <tombond248@gmail.com>
2022-10-10 18:51:26 +0100

    complete

--- Unsolved/script.js
+++ Unsolved/script.js
@@ -1,1 +1,17 @@
 //WRITE YOUR CODE BELOW
+var customerOrder = {
+    name: "coffee",
+    sugars: 2,
+    isReady: false,
+};
+
+console.log(customerOrder);
+
+if (customerOrder .isReady) {
+    console.log ('Ready for pick-up')
+} else {
+    console.log ('Still in order queue')
+}
+
+
+
